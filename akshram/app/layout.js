e3e5d48b6b25da6a5ef850f32d4f8891bf86da63
ak220193 <mail2meak22frcrio@gmail.com>
2025-08-01 10:15:13 +0530

    layout Created

--- akshram/app/layout.js
+++ akshram/app/layout.js
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Poppins } from 'next/font/google'
+import HeaderNav from '@/app/components/HeaderNav'
+import Footer from '@/app/components/footer'
 
 const poppins = Poppins({
   weight: ['300', '400', '500', '600', '700'],
@@ -16,7 +18,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={poppins.className}>
-        {children}
+        <HeaderNav />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   )
